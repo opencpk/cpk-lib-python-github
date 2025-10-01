f82508d9715e534bd7c7d3fbdde12a20cfd86c7c
hminaee-tc <hminaee@tucows.com>
2025-10-01 13:50:19 -0300

    fix: remove redundant folder and bring up one level up (#2)
    * fix: remove redundant folder and bring up one level up

--- cpk_lib_python_github/__init__.py
+++ cpk_lib_python_github/__init__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """CPK Lib Python GitHub - GitHub App Token Generator Package."""
 
-from .github_app_token_generator_package.github_app_token_generator import (
+from .github_app_token_generator import (
     token_manager,
 )
-from .github_app_token_generator_package.github_app_token_generator.config import (
+from .github_app_token_generator.config import (
     Config,
 )
-from .github_app_token_generator_package.github_app_token_generator.formatters import (
+from .github_app_token_generator.formatters import (
     OutputFormatter,
 )
-from .github_app_token_generator_package.github_app_token_generator.github_api import (
+from .github_app_token_generator.github_api import (
     GitHubAPIClient,
 )
 
